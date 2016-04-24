83e093e2c67479d3f7e9e43bef642af9c1433b9c
zurfyx <zurfyx@gmail.com>
2016-04-24 22:54:05 +0200

    landing page css

--- simple/config/urls.py
+++ simple/config/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^users', include('users.urls', namespace='users')),
     url(r'^projects', include('projects.urls', namespace='projects')),
-    url(r'^$', include('home.urls', namespace='home')),
+    url(r'^', include('home.urls', namespace='home')),
 ]
--- simple/users/urls.py
+++ simple/users/urls.py
@@ -4,6 +4,6 @@ from users.views import RegisterView, LoginView, AccountView
 
 urlpatterns = [
     url(r'^\/login', LoginView.as_view(), name='login'),
-    url(r'^\/signup', RegisterView.as_view(), name='register'),
+    url(r'^\/signup', RegisterView.as_view(), name='signup'),
     url(r'^\/(?P<pk>\d+)', AccountView.as_view(), name='account'),
 ]
