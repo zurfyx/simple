c6b64dd7f3bd08d2fa8e2b645861453be2534ba1
zurfyx <zurfyx@gmail.com>
2016-06-06 17:47:02 +0200

    project comments breadcrumb

--- simple/projects/comments/mixins.py
+++ simple/projects/comments/mixins.py
@@ -1,11 +1,20 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic.base import View
 
 from core.mixins import CustomLoginRequiredMixin, OwnerRequiredOrModeratorMixin, OwnerRequiredMixin
 from projects.comments.models import Comment
 from projects.models import Project
 
 
-class CommentAddMixin(CustomLoginRequiredMixin, CreateView):
+class CommentMixin(View):
+    def get_context_data(self, **kwargs):
+        context = super(CommentMixin, self).get_context_data(**kwargs)
+        context['project'] = get_object_or_404(Project, id=self.kwargs['project'])
+        return context
+
+
+class CommentAddMixin(CustomLoginRequiredMixin, CommentMixin, CreateView):
     model = Comment
 
     def form_valid(self, form):
@@ -14,7 +23,7 @@ class CommentAddMixin(CustomLoginRequiredMixin, CreateView):
         return super(CommentAddMixin, self).form_valid(form)
 
 
-class CommentEditMixin(OwnerRequiredMixin, UpdateView):
+class CommentEditMixin(OwnerRequiredMixin, CommentMixin, UpdateView):
     model = Comment
 
     def form_valid(self, form):
@@ -23,6 +32,6 @@ class CommentEditMixin(OwnerRequiredMixin, UpdateView):
         return super(CommentEditMixin, self).form_valid(form)
 
 
-class CommentDeleteMixin(OwnerRequiredOrModeratorMixin, DeleteView):
+class CommentDeleteMixin(OwnerRequiredOrModeratorMixin, CommentMixin, DeleteView):
     model = Comment
 
